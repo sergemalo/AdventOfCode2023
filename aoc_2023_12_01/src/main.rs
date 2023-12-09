2e3454ebd31f6c3045689cd4f3974cbe4ab0d497
Serge Malo <sergemalo2000@outlook.com>
2023-12-09 16:37:57 -0500

    my_rust_app contains code for AOC Dec 3 2023

--- aoc_2023_12_01/src/main.rs
+++ aoc_2023_12_01/src/main.rs
@@ -1,5 +1,22 @@
-use std::fs;
+use std::fs::File;
+use std::io::{self, prelude::*, BufReader};
 
-fn main() {
-    println!("Hello, world!");
+
+fn main() ->  io::Result<()> {
+    println!("Advent Of Code 2023 - 12 - 01 - By Serge Malo");
+
+    let file = File::open("input.txt")?;
+    let reader = BufReader::new(file);
+
+    for line in reader.lines() {
+        println!("{}", line?);
+        println!("{}", line?);
+    }
+
+
+
+
+
+    Ok(())
+
 }
--- my_rust_app/src/main.rs
+++ my_rust_app/src/main.rs
@@ -1,7 +1,11 @@
-use serde::Serialize;
-use std::io;
-use  std::fs;
+//use serde::Serialize;
+//use  std::fs;
+//use std::io;
+//use std::env;
+use std::fs;
+use regex::Regex;
 
+/* 
 
 #[derive(Serialize)]
 struct Record<'a> {
@@ -9,7 +13,6 @@ struct Record<'a> {
     place: &'a str,
     id: u64,
 }
-
 
 fn csv_example() -> Result<(), csv::Error> {
     let mut wtr = csv::Writer::from_writer(io::stdout());
@@ -27,23 +30,113 @@ fn csv_example() -> Result<(), csv::Error> {
     Ok(())
 
 }
+*/
 
-fn main() -> Result<(), csv::Error> {
-    //let _err =  csv_example();
+fn msg_wrap(s: &str) {
+
+    println!("Beau message: {}", s)
+
+}
+
+#[derive(Debug)]   // Indicate our class (Rectangle) implements the "Debug" trait
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
 
 
-    let mut file = fs::File::create("favorite_websites.txt")?;
-    file.write_all(b"opensource.com\n")?;
+fn main() {
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+    println!("rect1 is {:?}", rect1); // {:?} Indicate using the "debug" trait to format the output
+    dbg!(&rect1);
+
+    let msg = String::from("Mon message");
+
+    msg_wrap(&msg);
+
+    //panic!("crash and burn");
+
+    msg_wrap(&msg);
 
 
-    let file = fs::File::open("favorite_websites.txt")?;
-    let lines = io::BufReader::new(file).lines();
+    let file_path = String::from("2023-12-04-input-a.txt");
+    println!("In file {}", file_path);
+
+    let contents = fs::read_to_string(file_path)
+        .expect("Should have been able to read the file");
+
+    println!("With text:\n{contents}");
+
+    //for c in contents.chars() {
+    //    println!("-{}", c)
+    //}
     
-    for line in lines {
-        if let Ok(_line) = line {
-            println!(">>> {}", _line);
+    let re = Regex::new(r"^Card (\d+)\: (.+) \| (.+)$").unwrap();
+    let reWins = Regex::new(r"^\s*(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)(.*)$").unwrap();
+    let reNumbers = Regex::new(r"^\s*(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)(.*)$").unwrap();
+    let mut winning_nbs: [i32; 5];
+    let mut my_numbers: [i32; 8];
+    let mut total_points = 0;
+    let mut card_points = 0;
+
+    let split = contents.split('\n');
+    for line in split {
+        println!("zz ===> {}", line);
+        if re.is_match(line) {
+            let caps = re.captures(line).unwrap();
+            println!("C: {}", &caps[1]);
+
+            winning_nbs = [0; 5];
+            
+            if reWins.is_match(&caps[2]) {
+                let capsWins = reWins.captures(&caps[2]).unwrap();
+                winning_nbs[0] = capsWins[1].parse::<i32>().unwrap();
+                winning_nbs[1] = capsWins[2].parse::<i32>().unwrap();
+                winning_nbs[2] = capsWins[3].parse::<i32>().unwrap();
+                winning_nbs[3] = capsWins[4].parse::<i32>().unwrap();
+                winning_nbs[4] = capsWins[5].parse::<i32>().unwrap();
+            }
+
+            println!("Winning numbers: {:?}", winning_nbs);
+
+
+            my_numbers = [0; 8];
+            
+            if reNumbers.is_match(&caps[3]) {
+                let capsNbs = reNumbers.captures(&caps[3]).unwrap();
+                my_numbers[0] = capsNbs[1].parse::<i32>().unwrap();
+                my_numbers[1] = capsNbs[2].parse::<i32>().unwrap();
+                my_numbers[2] = capsNbs[3].parse::<i32>().unwrap();
+                my_numbers[3] = capsNbs[4].parse::<i32>().unwrap();
+                my_numbers[4] = capsNbs[5].parse::<i32>().unwrap();
+                my_numbers[5] = capsNbs[6].parse::<i32>().unwrap();
+                my_numbers[6] = capsNbs[7].parse::<i32>().unwrap();
+                my_numbers[7] = capsNbs[8].parse::<i32>().unwrap();
+            }
+
+            println!("My numbers: {:?}", my_numbers);
+
+            card_points = 0;
+            for win_nb in winning_nbs {
+                if my_numbers.contains(&win_nb) {
+                    if card_points == 0 {
+                        card_points = 1;
+                    }
+                    else {
+                        card_points *= 2;
+                    }
+
+                }
+            }
+
+            println!("Card Points:{}", card_points);
+            total_points += card_points;
         }
     }
 
-    Ok(())
+    println!("Total Points: {}", total_points);
+
 }
