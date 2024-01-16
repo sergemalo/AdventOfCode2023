858e9613f2869cada964e4ae8723ce07460b7621
Serge Malo <sergemalo2000@outlook.com>
2024-01-16 13:34:55 -0500

    AOC 2023-12-05 - Parsing Seeds with RegEx

--- aoc_2023_12_05/src/main.rs
+++ aoc_2023_12_05/src/main.rs
@@ -1,3 +1,26 @@
+use std::fs;
+use regex::Regex;
+
+/// .
 fn main() {
-    println!("Hello, world!");
+    println!("AOC 2023-12-05");
+
+    let file_path = String::from("2023-12-05-input-a.txt");
+    println!("In file {}", file_path);
+
+    let contents = fs::read_to_string(file_path)
+        .expect("Should have been able to read the file");
+
+    //println!("With text:\n{contents}");
+
+    let re_seeds_line = Regex::new(r"^seeds: (.+)$").unwrap();
+
+    let split = contents.split('\n');
+    for line in split {
+        println!("{line}");
+        if re_seeds_line.is_match(line) {
+            let caps = re_seeds_line.captures(line).unwrap();
+            println!("Seeds: {}", &caps[1]);
+        }
+    }
 }
